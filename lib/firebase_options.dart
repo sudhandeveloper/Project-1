cef2e46de51b608dea2aef28e7ef7cc40ecf766c
sudhandeveloper <111607666+sudhandeveloper@users.noreply.github.com>
2023-04-22 12:19:11 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -53,10 +53,10 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDlCvaZVP6vmXrwZ__FQzy7h6Fis8zRQ20',
-    appId: '1:512458523126:android:95974b3f64b0de5bd887d7',
-    messagingSenderId: '512458523126',
-    projectId: 'our-first-project-44556',
-    storageBucket: 'our-first-project-44556.appspot.com',
+    apiKey: "",
+    appId: '',
+    messagingSenderId: "",
+    projectId: "",
+    storageBucket: "",
   );
 }
